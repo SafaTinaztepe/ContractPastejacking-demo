eb1c1e30269eab0c3a50fc72646dd999ff56c71b
Safa Tinaztepe <safa.tinaztepe@gmail.com>
2021-12-19 14:46:54 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
 import './App.css';
+import { React, useEffect, useState } from "react";
+import Chain from "./Chain";
+
+let ADDRESS = "0x6356230A0Afb886dc79975FBa4a814f368A80A8F"
+let REPLACE_ADDRESS = "0x1b198ee33FBC1Ac94cA0c50AD8f12271d7E153C8"
+
+function TargetArea() {
+  return (
+    <div className="TargetArea">
+      Paste Field
+      <input style={{"marginLeft": "20px"}} type="text"></input>
+    </div>
+  )
+}
+
+function SourceArea({address}) {
+  
+  return (
+    <div>
+      <div id="source-contract">
+        <span>{address}</span>
+      </div>
+      <br/>
+      <span><strong>Or use your own address</strong></span>
+      <br/>
+      <br/>
+      <button>Use My Address</button>
+    </div>
+  )
+}
+
+function handleCopy(event) {
+  console.log("copy event detected", event)
+  event.clipboardData.setData("text/plain", REPLACE_ADDRESS)
+  event.preventDefault()
+}
+
+const InstructionStep = (props) => {
+  return (
+    <div className='InstructionStep'>
+      <h2 className={'head' + ' ' + (props.i % 2 ? 'text-left' : 'text-right')}>
+        {props.i}. {props.text} 
+      </h2>
+      <h4>{props.subtext}</h4>
+      {props.children}
+    </div>
+  )
+}
 
 function App() {
+
+  let [sourceAddress, setSourceAddress] = useState(ADDRESS)
+
+  useEffect(() => {
+    document.getElementById("source-contract")
+            .addEventListener("copy", handleCopy)
+    return () => {}
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Malicious Site
       </header>
+      <div>
+        <div className="InstructionField">
+          <InstructionStep 
+            i={0} 
+            text={"Connect to the Ethereum Blockchain"}
+            subtext={"Copy this address"}
+          >
+            <Chain filterAttributes={["Activate", "GetAccount"]} />
+          </InstructionStep>
+
+          <InstructionStep 
+            i={1} 
+            text={"Source Address"}
+            subtext={"Copy this address"}
+          >
+            <SourceArea address={ADDRESS}/>
+          </InstructionStep>
+
+          <InstructionStep 
+            i={2} 
+            text={"Paste Target"}
+            subtext={"Paste what you copied here and compare the difference"}
+          >
+            <TargetArea />
+          </InstructionStep>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Chain.js
@@ -0,0 +1,211 @@
+
+import React, { useState, useEffect } from "react";
+
+import { InjectedConnector } from '@web3-react/injected-connector'
+import {
+  Web3ReactProvider,
+  useWeb3React,
+  UnsupportedChainIdError
+} from "@web3-react/core";
+
+import { Web3Provider } from "@ethersproject/providers";
+import { Contract } from "@ethersproject/contracts";
+import { formatEther } from '@ethersproject/units'
+import { getContractAddress } from '@ethersproject/address';
+import { BigNumber } from '@ethersproject/bignumber';
+
+// import '../Messages/Message/Message.css';
+
+
+const getLibrary = (provider) => {
+    const library = new Web3Provider(provider); // resolved in the browser
+    library.pollingInterval = 15000;
+    return library;
+}
+
+const injected = new InjectedConnector({ supportedChainIds: [1, 3, 4, 5, 42] })
+
+const Chain = (props) => {
+  console.log(props)
+    return (
+        <Web3ReactProvider getLibrary={getLibrary}>
+            <_Chain filterAttributes={props.filterAttributes} setContract={props.setContract}/>
+        </Web3ReactProvider>
+    )
+}
+
+const _Chain = (props) => {
+    const context = useWeb3React();
+    let { 
+      chainId, 
+      account, 
+      library, 
+      activate, 
+      deactivate, 
+      active 
+    } = context;
+    
+    let [blockNumber, setBlockNumber] = useState();
+    let [ethBalance, setEthBalance] = useState();
+    let [contract, setContract] = useState();
+
+
+    const updateBlockNumber = (blockNumber) =>  {
+      setBlockNumber(blockNumber);
+    };
+    const updateEthBalance = (balance) =>  {
+      setEthBalance(balance);
+    };
+    const getBlockNumber = () => {
+      if (library){
+        let stale = false;
+        library
+          .getBlockNumber()
+          .then(blockNumber => {
+            if (!stale) {
+              updateBlockNumber(blockNumber);
+              // props.setBlockNumber(blockNumber);
+            }
+          })
+          .catch(() => {
+            if (!stale) {
+              updateBlockNumber(null);
+            }
+          });
+        console.log(blockNumber);
+        return blockNumber;
+      }
+    }
+    const getEthBalance = () => {
+      if (library && account) {
+        let stale = false;
+    
+        library
+          .getBalance(account)
+          .then(balance => {
+            if (!stale) {
+              // console.log(formatEther(balance))
+              setEthBalance(formatEther(balance));
+            }
+          })
+          .catch(() => {
+            if (!stale) {
+              setEthBalance(null);
+            }
+          });
+          
+        return () => {
+          stale = true;
+          setEthBalance(undefined);
+        };
+      }
+    }
+    const activateConnector = () => {
+      activate(injected)
+      console.log(active)
+    }
+    const deactivateConnector = () => {
+      deactivate(injected)
+      console.log(active)
+    }
+
+    const getAccount = () => {
+      console.log(account)
+      return account
+    }
+
+    // const getContract = () => {
+    //   if (library) {
+    //     const signer = library.getSigner(account)
+    //     var asyncAddress = "0x4F37310372dd39d451f7022EE587FA8B9F72d80B" // contract address
+    //     const _contract = new Contract(asyncAddress, AsyncAbi, signer)
+    //     setContract(_contract)
+    //     props.setContract(_contract)
+    //   }
+    // }
+
+    const ownerOfToken = async (token) => {
+      if (contract) {
+        // contract.ownerOf(BigNumber.from(732)).then((data) => {console.log(data)}).catch((err) => {console.log(err)})
+        contract.ownerOf(BigNumber.from(token)).then((data) => {console.log(data)}).catch((err) => {console.log(err)})
+        // console.log(owner)
+      }
+    }
+
+    const valueOfToken = async (token) => {
+      if (contract) {
+        // contract.ownerOf(BigNumber.from(732)).then((data) => {console.log(data)}).catch((err) => {console.log(err)})
+        contract.getControlToken(BigNumber.from(token))
+                .then((data) => {console.log(data)})
+                .catch((err) => {console.log(err)})
+        // console.log(owner)
+      }
+    }
+
+    useEffect(() => {
+      // if(!contract){
+      //   getContract()
+      // }
+    })
+
+    const chainOptions = {
+      "Connected": () => {  },
+      "Activate": activateConnector,
+      "Deactivate": deactivateConnector,
+      "BlockNumber": getBlockNumber,
+      "Balance": activateConnector,
+      "Contract": () => {console.log(contract)},
+      "Owner": ownerOfToken,
+      "GetAccount": getAccount
+    }
+
+    const networkMap = {
+      1: "Mainnet",
+      3: "Ropsten",
+      4: "Rinkeby",
+      5: "Goerli",
+      2018: "dev",
+      61: "Classic",
+      63: "Mordor",
+      6: "Kotti",
+      212: "Astor",
+    }
+    let a = Object.keys(chainOptions)
+    .filter((attribute) => {
+      console.log("GetAccount" in props.filterAttributes)
+      return attribute in props.filterAttributes
+    })
+    console.log(a)
+    
+    return (
+      <>
+        <h4>
+          Connected? {
+          active ? 
+            <div>
+              <span className="emoji">✅ </span>
+              <span>Network: {networkMap[chainId]}</span>
+              {console.log(chainId)}
+              <button onClick={deactivateConnector}>Disconnect</button>
+            </div> 
+            : 
+            <div>
+              <span className="emoji">❌ </span>
+              <button onClick={activateConnector}>Connect</button>
+            </div> 
+          }
+        </h4>
+        {/* <ul>
+          {
+          Object.keys(chainOptions)
+                // .filter((attribute) => attribute in props.filterAttributes)
+                .map((option,i) => {
+                  return(<li key={i}><button onClick={chainOptions[option]} className="messageBox backgroundOrange"><span className="messageText">{option}</span></button></li>)
+                })
+          }
+        </ul> */}
+      </>
+    )
+}
+
+export default Chain;
